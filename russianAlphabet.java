021e8219f36532070a9f3c580aa50d3c0bdbb568
Benhap1 <116707057+Benhap1@users.noreply.github.com>
2023-02-13 18:19:27 +0300

    Create russianAlphabet.java

---
+++ russianAlphabet.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1040; i < 1046; i++) {
+            char c = (char) i;
+            System.out.println(c);
+        }
+        char ascii = (char) 1025;
+        System.out.println(ascii);
+
+        for (int i = 1046; i < 1078; i++) {
+            char b = (char) i;
+            System.out.println(b);
+        }
+        char ascii2 = (char) 1105;
+        System.out.println(ascii2);
+
+        for (int i = 1078; i < 1104; i++) {
+            char a = (char) i;
+            System.out.println(a);
+        }
+    }
+}
